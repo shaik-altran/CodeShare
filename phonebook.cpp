e235ba19470eda739873929942205d20a373f738
Anushak07 <97anusha.k@gmail.com>
2022-02-22 14:37:49 +0530

    phonebook with private members of class

--- phonebook.cpp
+++ phonebook.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 static int i=0;
 class contact
@@ -25,93 +26,73 @@ class contact
 			number=num;
 			return number;
 		}
-		void editContact(contact *con[],string editcontact,string modifycontact)
-                {
-
-
-                        for(int j=0;j<i;j++)
-                        {
-                                if(editcontact==con[j]->getName())
-                                {
-                                        con[j]->setName(modifycontact);
-                                }
-                                if(editcontact==con[j]->getNumber())
-                                {
-                                        con[j]->setNumber(modifycontact);
-                                }
-                        }
-                }
 
 };
 class phonebook
 {
-        public:
-                void addNewContact(contact *con[],string nam,string numb)
+	contact *contactObj[10];
+
+	public:
+                void addNewContact(string nam,string numb)
                 {
-                        cout<<"in add!"<<endl;
-			cout<<"i="<<i<<endl;
-                        con[i]->setName(nam);
-                        con[i]->setNumber(numb);
+			contactObj[i]={new contact};	
+			contactObj[i]->setName(nam);
+                        contactObj[i]->setNumber(numb);
                         i++;
-			cout<<"addedd"<<endl;
-	               	delete con[i];         
+		}
 
-                }
-
-                void displayContact(contact *con[])
+                void displayContact()
                 {
 
-                        cout<<"in display!"<<endl;
                         for(int j=0;j<i;j++)
                         {
                                 cout<<"name:";
-                                cout<<con[j]->getName();
+                                cout<<contactObj[j]->getName();
                                 cout<<"   ";
                                 cout<<"number:";
-                                cout<<con[j]->getNumber()<<endl;
+                                cout<<contactObj[j]->getNumber()<<endl;
                         }
                 }
-                void searchContact(contact *con[],string searchcontact)
+		void searchContact(string searchcontact)
                 {
                         for(int j=0;j<i;j++)
                         {
-                                if(searchcontact==con[j]->getName()||searchcontact==con[j]->getNumber())
+                                if(searchcontact==contactObj[j]->getName()||searchcontact==contactObj[j]->getNumber())
                                 {
                                         cout<<"name:";
-                                        cout<<con[j]->getName();
+                                        cout<<contactObj[j]->getName();
                                         cout<<" ";
                                         cout<<"number:";
-                                        cout<<con[j]->getNumber()<<endl;
+                                        cout<<contactObj[j]->getNumber()<<endl;
                                 }
                         }
                 }
-
-		void deleteContact(contact *con[],string deletecontact)
+		
+		void deleteContact(string deletecontact)
                 {
                         int pos=0;
                         for(int j=0;j<i;j++)
                         {
 
-                                if(deletecontact==con[j]->getName()||deletecontact==con[j]->getNumber())
+                                if(deletecontact==contactObj[j]->getName()||deletecontact==contactObj[j]->getNumber())
                                 {
-                                        for(j=pos;j<i-1; j++)
+                                        
+					for(j=pos;j<i-1; j++)
                                         {
-                                                con[j]=con[j+1];
+                                	 	contactObj[j]=contactObj[j+1];
                                         }
                                         i--;
+
                                 }
                                 pos++;
                         }
 
                 }
-
 };
 
 int main()
 {
         phonebook p;
-	contact *con[10]={new contact()};
-	string *ptr=(string *)con;
 	contact c;
 	int choice;
         while(1)
@@ -128,44 +109,37 @@ int main()
                         cin>>name;
                         cout<<"enter phno:";
                         cin>>number;
-                        p.addNewContact(con,name,number);
+                        p.addNewContact(name,number);
 
                 }
                 else if(choice==2)
                 {
-                        p.displayContact(con);
+                        p.displayContact();
                 }
                 else if(choice==3)
                 {
                         string searchstring;
                         cout<<"enter the contact to search:";
                         cin>>searchstring;
-                        p.searchContact(con,searchstring);
+                        p.searchContact(searchstring);
                 }
-                else if(choice==4)
-                {
-                        string editstring;
-                        string modifystring;
-                        cout<<"enter the contact to edit:";
-                        cin>>editstring;
-                        cout<<"enter the new name:";
-                        cin>>modifystring;
-                        c.editContact(con,editstring,modifystring);
-                }
-                else if(choice==5)
+		else if(choice==5)
                 {
                         string deletestring;
                         cout<<"enter the contact to delete:";
-			cin>>deletestring;
-                        p.deleteContact(con,deletestring);
+                        cin>>deletestring;
+                        p.deleteContact(deletestring);
                 }
-                else if(choice==6)
+
+		else if(choice==6)
                 {
                         exit(0);
                 }
                 else
                         printf("wrong choice!!!\n");
 
-        }
+	}
+
+               
 }
 
